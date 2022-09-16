df75cd0ef05c271504eb16e9d29f3e83c79dff20
MZuenni <michi.zuendorf@gmail.com>
2022-09-16 18:34:19 +0200

    improved example

--- example/outputValidator.cpp
+++ example/outputValidator.cpp
@@ -1,6 +1,9 @@
 //============================================================================//
 // validate.h example usage                                                   //
 //============================================================================//
+// Note BAPCtools also invokes the outputValidator in a strict mode i.e. case
+// and space sensitive to validate the jury answer. To make this not fail you
+// need to correctly validate the whitespaces!
 
 #include "../src/validate.h"
 
@@ -10,15 +13,15 @@ int main(int argc, char **argv) {
 
 	if (stringEqual(juryAns.string(), "impossible")) {	// check if the test has an answer
 		teamAns.expectString("impossible");				// check if the participant found no answer either
-		teamAns.newline();								// typically this does nothing since output checking is not space sensitive
+		teamAns.newline();								// only relevant when validating the jury answer
 		teamAns.eof();									// check for garbage
-		juryOut << "OK" << AC;
+		juryOut << "OK impossible" << AC;
 	} else {
 		Integer n = testIn.integer();					// get the testdata
 		Integer ans = teamAns.integer();				// get team answer
-		teamAns.newline();
+		teamAns.newline();                              // only relevant when validating the jury answer
 		if (2*ans != n) juryOut << "wrong answer" << WA;
-		teamAns.eof();
-		juryOut << "OK" << AC;
+		teamAns.eof();                                  // check for garbage
+		juryOut << "OK possible" << AC;
 	}
 }
