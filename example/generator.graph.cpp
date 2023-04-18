53380a677ab6c8f91b03ff983dc14305e7a31107
mzuenni <michi.zuendorf@gmail.com>
2023-04-18 13:47:56 +0200

    improved example

--- example/generator.graph.cpp
+++ example/generator.graph.cpp
@@ -21,12 +21,19 @@ int main(int argc, char **argv) {
 	res.addEdge(17, 9);
 	res.addEdge(18, 10);
 
-	// mage the graph use vertices [1, 20)
-	res.setMinId(1);
 	// randomly permutate edges and vertices
 	res.permutate();
+	// random DAG (since the vertex order is random)
+	DiGraph<NoData> dag = res.directed([](Integer from, Integer to){
+		return from < to;
+	});
+
+	// mage the graph use vertices [1, 20)
+	dag.setMinId(1);
+	// randomly permutate edges and vertices
+	dag.permutate();
 	// print:
 	// nodeCount EdgeCount
 	// edges...
-	testOut << res << std::endl;
+	testOut << dag << std::endl;
 }
