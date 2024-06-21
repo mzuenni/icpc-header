3b69a2a790d32ea66aa36af77b7f0eb7d8c256a1
MZuenni <michi.zuendorf@gmail.com>
2024-06-21 18:20:50 +0200

    test

--- test/validate.cpp
+++ test/validate.cpp
@@ -10,7 +10,7 @@ namespace Settings {
 }
 #include "../src/validate.h"
 
-#if __cpp_lib_source_location >= 201907L
+#if __cplusplus > 202000L
 #include <source_location>
 #else
 namespace std {
@@ -21,8 +21,7 @@ namespace std {
 		constexpr const char* file_name() const noexcept {return "";}
 		constexpr const char* function_name() const noexcept {return "";}
 	};
-#endif
-#if __cpp_lib_ssize < 201902L
+
 	template <class C>
 	constexpr auto ssize(const C& c) -> std::common_type_t<std::ptrdiff_t, std::make_signed_t<decltype(c.size())>> {
 		using R = std::common_type_t<std::ptrdiff_t, std::make_signed_t<decltype(c.size())>>;
