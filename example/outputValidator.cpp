fdd023db52a143bf9ddc828845b3b4e612502dae
mzuenni <michi.zuendorf@gmail.com>
2025-06-11 23:42:50 +0200

    improve output validator example

--- example/outputValidator.cpp
+++ example/outputValidator.cpp
@@ -4,27 +4,50 @@
 // Note BAPCtools also invokes the outputValidator in a strict mode i.e. case
 // and space sensitive to validate the jury answer. To make this not fail you
 // need to correctly validate the whitespaces!
+// Therefore, it is best practice to use the same method to parse team and jury
+// input. This has the added benefit that errors in the jury answer can be
+// noticed and reported with FAIL (instead of the team silently gettting WA).
 
 #include "../src/validate.h"
-using namespace OutputValidator;						// we are writing an output validator
+using namespace OutputValidator;							// we are writing an output validator
+
+bool verify(Integer n) {
+	return n == 6;
+}
+
+// same method to parse/verify team and jury input
+std::string parseAndVerify(InputStream& in, std::string_view who, Verdict onError) {
+	std::regex options = caseSensitive.regex("Yes|No");		// regex to check possible answers
+	std::string ans = in.string(options);					// read answer
+	teamAns.newline();										// check whitespaces
+
+	if (stringEqual(ans, "Yes")) {							// if the answer is yes
+		Integer n = in.integer();							// read the solution
+		if (!verify(n)) {									// and verify it
+			juryOut << who << " solution is invalid!" << onError;
+		}
+		in.newline();										// check whitespaces
+	}
+	in.eof();												// check for garbage
+	return ans;
+}
 
 int main(int argc, char **argv) {
-	OutputValidator::init(argc, argv);					// initialize streams, rng and parse arguments
+	OutputValidator::init(argc, argv);						// initialize streams, rng and parse arguments
 
-	if (stringEqual(juryAns.string(), "impossible")) {	// check if the test has an answer
-		teamAns.expectString("impossible");				// check if the participant found no answer either
-		teamAns.newline();								// only relevant when validating the jury answer
-		teamAns.eof();									// check for garbage
-		teamOut << "OK impossible" << AC;
+
+	std::string j = parseAndVerify(juryAns, "jury", FAIL);	// read jury answer (errors result in FAIL)
+	std::string t = parseAndVerify(teamAns, "team", WA);	// read team answer (errors result in WA)
+
+	if (stringEqual(j, t)) {								// check if the answers are equal
+		teamOut << "OK same answer" << AC;
+	} else if (stringEqual(t, "Yes")) {						// and check that team is not better than jury
+		juryOut << "team found solution but jury not!" << FAIL;
+	} else if (stringEqual(j, "Yes")) {
+		juryOut << "team did not print 6?!" << std::endl;
+		teamOut << "jury found solution but team not!" << WA;
 	} else {
-		Integer n = testIn.integer();					// get the testdata
-		Integer ans = teamAns.integer();				// get team answer
-		teamAns.newline();								// only relevant when validating the jury answer
-		if (2*ans != n) {
-			juryOut << "should be 2*n" << std::endl;	// a message only the jury can see 
-			teamOut << "wrong answer" << WA;			// a message that the team (if enabled for samples) and jury can see
-		}
-		teamAns.eof();									// check for garbage
-		teamOut << "OK possible" << AC;
+		juryOut << "how did this happen?!" << FAIL;
 	}
+	return AC;
 }
--- example/outputValidator2.cpp
+++ example/outputValidator2.cpp
@@ -8,42 +8,23 @@
 #include "../src/validate.h"
 using namespace OutputValidator;						// we are writing an output validator
 
-bool verify(Integer n) {
-	return n == 6;
-}
-
 int main(int argc, char **argv) {
 	OutputValidator::init(argc, argv);					// initialize streams, rng and parse arguments
 
-	std::regex options = caseSensitive.regex("Yes|No");	// regex to check possible answers
-	std::string j = juryAns.string(options);			// read jury answer
-	std::string t = teamAns.string(options);			// read team answer
-	teamAns.newline();									// only relevant when validating the jury answer
-
-	if (stringEqual(j, "Yes")) {						// if the answer is yes
-		Integer n = juryAns.integer();					// read the solution
-		if (!verify(n)) {								// and verify it (should always be correct!)
-			juryOut << "jury solution is invalid!" << FAIL;
-		}
-	}
-
-	if (stringEqual(t, "Yes")) {						// if the answer is yes verify the solution
-		Integer n = juryAns.integer();					// read the solution
-		teamAns.newline();								// maybe check whitespaces
-		if (!verify(n)) {								// and verify it (could be wrong)
-			teamOut << "team solution is invalid!" << WA;
-		}
-	}
-	teamAns.eof();										// check for garbage
-
-	if (stringEqual(j, t)) {							// check if the answers are equal
-		teamOut << "OK same answer" << AC;
-	} else if (stringEqual(t, "Yes")) {					// and check that team is not better than jury
-		juryOut << "team found solution but jury not!" << FAIL;
-	} else if (stringEqual(j, "Yes")) {
-		juryOut << "team did not print 6?!" << std::endl
-		teamOut << "jury found solution but team not!" << WA;
+	if (stringEqual(juryAns.string(), "impossible")) {	// check if the test has an answer
+		teamAns.expectString("impossible");				// check if the participant found no answer either
+		teamAns.newline();								// only relevant when validating the jury answer
+		teamAns.eof();									// check for garbage
+		teamOut << "OK impossible" << AC;
 	} else {
-		juryOut << "how did this happen?!" << FAIL;
+		Integer n = testIn.integer();					// get the testdata
+		Integer ans = teamAns.integer();				// get team answer
+		teamAns.newline();								// only relevant when validating the jury answer
+		if (2*ans != n) {
+			juryOut << "should be 2*n" << std::endl;	// a message only the jury can see 
+			teamOut << "wrong answer" << WA;			// a message that the team (if enabled for samples) and jury can see
+		}
+		teamAns.eof();									// check for garbage
+		teamOut << "OK possible" << AC;
 	}
 }
